01d5031740e9d1a7133c0a8656c548ca79d871f1
Nurul Farah Hani binti Zaimal <nurul.f.binti.zaimal@accenture.com>
2021-08-13 16:06:13 +0800

    Rearranged folder, added logo & resize font

---
+++ src/components/main/main.js
@@ -0,0 +1,12 @@
+import FontSelection from "../font_selection/font_selection";
+import TiaraLogo from "../../assets/TiaraLogo.png";
+import style from "./main.module.scss";
+
+export default function Main() {
+  return (
+    <div>
+      <img className={style.logo} src={TiaraLogo} alt="TiaraLogo" />
+      <FontSelection />
+    </div>
+  );
+}
